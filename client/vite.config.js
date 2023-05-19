04c7ef6573b3b99047758439d3dc47578b772e60
josh maddox <maddoxjosh310@yahoo.com>
2023-05-19 10:37:23 -0400

    deleted vite files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
+ // https://vitejs.dev/config/
+ //https://vitejs.dev/config/server-options.html#server-proxy for example for mulitple Router
+ export default defineConfig({
+  server: {
+    proxy: {
+      // put target start params example localhost:9000/test
+      '/Target start params here ': {
+        target: 'http://localhost:{Port Number Here}/',
+        changeOrigin: true,
+      },
+    }
+  },
   plugins: [react()],
 })
