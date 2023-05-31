f0be677e27e3deb5b4b5519ec47e70b3562e9d3b
mathew macias <mattmacias.mm@gmail.com>
2023-05-30 22:31:15 -0700

    update form code + initial commit on saved likes

--- client/src/components/AddLikesForm.jsx
+++ client/src/components/AddLikesForm.jsx
@@ -6,28 +6,31 @@ import NavBar from "./Navbar";
 
 export default function AddLikesform(props){
     
-    const initInputs = {
-        moviePoster:props.moviePoster || "",
-        title:props.title || "",
-        type:props.type || "",
-        genre:props.genre || ""
+const initInputs = {
+    moviePoster:props.moviePoster || "",
+    title:props.title || "",
+    type:props.type || "",
+    genre:props.genre || ""
         
-    }
-    //input form state 
-    const [inputs,setInputs] = useState(initInputs)
+}
+//input form state 
+const [inputs,setInputs] = useState(initInputs)
 
-    function handleChange(e){
+//makes all input changes controlled by react 
+function handleChange(e){
         const {name ,value} = e.target
-        setInputs(prevInputs =>({...prevInputs, [name]:value}))
+        setInputs(prevInputs =>({...prevInputs, 
+            [name]:value}))
     }
-// adds the Movie or tv show 
+ //submit 
 function handleSubmit(e){
     e.preventDefault()
-    //post request
-    props.submit(inputs, props._id)
+    props.submit(inputs, props._id)//inputs is the new movie object state and props._id is coming from the likelist prop
     setInputs(initInputs)
 }
 console.log('inputs',inputs)
+
+const defaultImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtRcLq69MMpy4uCtnR8azqxnIxOVlSqVlhSg&usqp=CAU"
 
     return(
         <>
@@ -39,7 +42,7 @@ console.log('inputs',inputs)
             name='moviePoster' 
             value={inputs.moviePoster} 
             onChange={handleChange} 
-            placeholder='Add Movie poster'/>
+            placeholder='Add Movie poster (imgUrl)'/>
             
             <input 
             type='text' 
@@ -50,19 +53,29 @@ console.log('inputs',inputs)
             
             <select name="type" id="type" onChange={handleChange}>
                 <option>---Type---</option>
-                <option value="movie">Movie</option>
-                <option value="tv-show">Tv Show</option>
+                <option value={"movie"}>Movie</option>
+                <option value={"tv-show"}>Tv Show</option>
             </select>
 
             <select name="genre" id="genre" onChange={handleChange}>
                 <option>---Genre---</option>
-                <option value="action">Action</option>
-                <option value="horror">Horror</option>
-                <option value="comedy">Comedy</option>
-                <option value="fantasy">Fantasy</option>
+                <option value={"action"}>Action</option>
+                <option value={"horror"}>Horror</option>
+                <option value={"comedy"}>Comedy</option>
+                <option value={"fantasy"}>Fantasy</option>
             </select>
+            <button>{props.btnText}</button>
 
-            <button> Add Like </button>
+            <div className="previewBox">
+            {inputs.moviePoster === "" ? (
+            <img src={defaultImg} style={{ width: "250px", height: "275px" }} />
+            ) : (
+            <img src={inputs.moviePoster} style={{ width: "250px", height: "275px" }} />
+            )}
+                <h2>Title:{inputs.title}</h2>
+                <h2>Type:{inputs.type}</h2>
+                <h2>Genre:{inputs.genre}</h2>
+            </div>
 
         </form>
         </>
--- client/src/components/SavedLikes.jsx
+++ client/src/components/SavedLikes.jsx
@@ -2,13 +2,47 @@
 import React,{useState} from "react";
 import "./SavedLikes.css"
 import NavBar from "./Navbar";
+import AddLikesform from "./AddLikesForm";
 
 export default function SavedLikes(props){
-    
+    //state for edit btn toggle
+const [editToggle,setEditToggle] = React.useState(true)
+
+//props used 
+const {likesList,editLikes,deleteLikes} = props
+
+//map out data to dom
+const likedMovieElements = likesList.map(item => (
+<div key={item._id} className="likedItemBox">
+    {editToggle ? 
+    <>
+    <img src = {item.moviePoster} style={{width: '150px', height: '200px' }}/>
+    <h3>{item.title}</h3>
+    <h3>{item.type}</h3>
+    <h3>{item.genre}</h3>
+    <button className="deleteBtn" onClick={()=>deleteLikes(item._id)}>X</button>
+    <button className="editBtn" onClick={()=> setEditToggle(prevToggle=>!prevToggle)}>Edit</button>
+    </>
+    :
+    <>
+    <AddLikesform
+    moviePoster={item.moviePoster}
+    title={item.title}
+    type={item.type}
+    genre={item.genre}
+    _id={item._id}
+    btnText = "Submit Edit"
+    submit = {editLikes}
+    />
+    <button onClick={()=>setEditToggle(prevToggle => !prevToggle)}>Close</button>
+    </>
+    }
+</div>))
 
     return(
     <>
     <NavBar/>
+    {likedMovieElements}
     </>
     )
 }
